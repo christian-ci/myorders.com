2b5a3495e26261046bba843b99dd11cad63b29d0
quantumtheory78 <quantumtheory78@gmail.com>
2021-03-21 21:07:00 -0400

    Created Role Policy for EC2 S3 access and EC2 Role

--- main.tf
+++ main.tf
@@ -242,18 +242,48 @@ module "rds" {
     create_monitoring_role = true
     monitoring_interval = 60
 }
+data "aws_caller_identity" "current" {}
 
-module "privates3" {
-    source = "terraform-aws-modules/s3-bucket/aws"
-    version = ">=1.21.0"
-
-
-    
+resource "aws_iam_policy" "s3_role" {
+  name = "Policy-S3-access"
+  description = "Policy for role S3 access"
+  policy = jsonencode (
+    {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "S3Policy",
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:ListBucket",                
+            ],
+            "Resource": [
+                "arn:aws:s3:::${local.bucket_name}",
+                "arn:aws:s3:::${local.bucket_name}/*",
+            ]
+        }
+    ]
 }
-# data "aws_iam_policy_document" "bucket_policy" {
-#   statement {
-#     sid       = "s3-bucket-policy"
-#     actions   = ["s3:PutObject", "s3:GetObjectAcl", "s3:GetObject", "s3:listbucket",]
-#     resources = [""]
-#   }
-# }
+  )
+
+}
+
+resource "aws_iam_role" "s3_role_access" {
+  name = "ec2-s3-role-access"
+   assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+  managed_policy_arns = [aws_iam_policy.s3_role.arn]
+}
